af5b8e70416c2f02d9fa981380dea571d5b4cbc2
kaustubhkulkarni1509 <kaustubh.kulkarni@sjsu.edu>
2020-01-18 22:32:12 -0800

    Adding Ride Address Model

---
+++ backend/src/dbmodels/rideAddress.js
@@ -0,0 +1,25 @@
+const {sequelize, DataTypes} = require('../db/conn')
+
+const RideAddress = sequelize.define('Ride_Address',{
+    RA_S_Id:{
+        type:DataTypes.STRING
+    }, 
+    RA_R_Id:{
+        type:DataTypes.INTEGER
+    }, 
+    RA_Street:{
+        type:DataTypes.STRING
+    }, 
+    RA_City:{
+        type:DataTypes.STRING
+    }, 
+    RA_State:{
+        type:DataTypes.STRING
+    }, 
+    RA_Zip:{
+        type:DataTypes.INTEGER
+    }
+},{
+    timestamps:false,
+    tableName:'Ride_Address'
+})
