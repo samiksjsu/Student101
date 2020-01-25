9427e9e1f68fe5ec9ebc4b77b29ddf941bfd229b
samikdada <samikdada@gmail.com>
2020-01-24 22:47:48 -0800

    app.js

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,25 @@
-console.log(new Date().getFullYear() + '-' + new Date().getMonth() + 1 + '-' + new Date().getDate())
+const Student = require('./src/dbmodels/student')
+const arr = ['014500889', '014500890']
+
+
+
+async function getEmails(arr) {
+    var emails = ''
+    for(const id of arr) {
+        var temp = await Student.findOne({
+            where: {
+                S_Id: id
+            }
+        })
+
+        emails += temp.dataValues.S_Email + ','
+    }
+    return emails
+}
+
+getEmails(arr).then((emails) => {
+    emails = emails.replace(/(^,)|(,$)/g, "")
+    console.log(emails)
+}).catch((e) => {
+    console.log(e)
+})
